9e94070a57290c73ac1039376f61c2c524ce8937
adamhsparks <adamhsparks@gmail.com>
2016-07-31 10:11:12 +1000

    Add vignette changes to Pull data from SILO

--- R/plot_validations.R
+++ R/plot_validations.R
@@ -11,8 +11,13 @@
 #'
 #' @export
 #'
-
-merged <- NULL
-ggplot2::ggplot(merged, ggplot2::aes(x = SILO_TMP, y = BoM_TMP)) +
-  ggplot2::stat_binhex() +
-  ggplot2::geom_abline(slope = 1, colour = "white")
+plot_density <- function(BoM_SILO_data) {
+  BoM_SILO_data <- NULL
+  
+  ggplot2::ggplot(BoM_SILO_data, ggplot2::aes(x = SILO_TMP, y = BoM_TMP)) +
+    ggplot2::stat_binhex() +
+    ggplot2::geom_abline(slope = 1, colour = "white") +
+    ggplot2::xlab("SILO Downscaled (Predicted, ˚C)") +
+    ggplot2::ylab("BoM Hourly (Actual Values, ˚C)") +
+    ggplot2::ggtitle("Density of Predicted Versus Actual Values")
+}
